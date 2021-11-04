2f50064a0fefcd66df66e7796cdf731931bb28c1
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2021-11-04 17:57:52 +0700

    unknown extra files

---
+++ filmmaking_app/lib/imageselectdialog [conflict].dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'classes.dart';
+
+typedef ImageSelectCallback = void Function(ImageData, int);
+
+class ImageSelectDialog extends StatelessWidget {
+  final ImageSelectCallback onTap;
+  final List<ImageData> images;
+  final List<AssetImage> imageCache;
+
+  ImageSelectDialog({Key key, @required this.images, @required this.imageCache, @required this.onTap})
+      : super(key: key);
+
+  // TODO: Centralize image caches to one central Store
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4),
+      ),
+      elevation: 0.0,
+      backgroundColor: theme.cardColor,
+      child: (imageCache != null && imageCache.length > 0)
+          ? ListView.builder(
+              shrinkWrap: true,
+              itemCount: imageCache.length,
+              itemBuilder: (BuildContext context, int i) => ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 32),
+                leading: CircleAvatar(
+                  backgroundImage: imageCache[i],
+                  foregroundColor: theme.secondaryHeaderColor,
+                ),
+                title: Text(
+                  images[i].label,
+                  style: theme.textTheme.bodyText1,
+                ),
+                onTap: () {
+                  onTap(images[i], i);
+                  Navigator.pop(context);
+                },
+              ),
+            )
+          : SizedBox(
+              height: 200,
+              child: Center(
+                child: Text('Nothing here', style: theme.textTheme.bodyText2),
+              ),
+            ),
+    );
+  }
+}
